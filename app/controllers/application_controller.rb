3dd3fb00af616179fcdc3ccb9302cb82d7aebe5b
mtrimner <matt.trimner@gmail.com>
2020-10-07 12:13:59 -0700

    added git ignore and changed secret to env file

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,7 +7,7 @@ class ApplicationController < Sinatra::Base
     set :views, 'app/views'
     enable :sessions
     set :sessions, true
-    set :session_secret, "secret"
+    set :session_secret, ENV["SECRET"]
     register Sinatra::Flash
   end
 
